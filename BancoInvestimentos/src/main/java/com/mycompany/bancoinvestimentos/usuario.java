eb6bd8c6c59b66ddd78966d4e99b973f76a3e895
CleitonJB <cleitonbraga56@gmail.com>
2017-09-14 15:27:25 -0300

    eu amo isso

---
+++ BancoInvestimentos/src/main/java/com/mycompany/bancoinvestimentos/usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bancoinvestimentos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class usuario {
+    private string nome;
+    private int cpf;
+    private int rg;
+    private Date datadenascimento;
+    private int codigodousuario
+    
+    
+}
